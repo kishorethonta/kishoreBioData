3780f3049ba6b6e2a377612fcff426551a7f6cb7
alpha-knowledge <alphaknowledgeofficial@gmail.com>
2026-02-28 12:51:34 +0530

    new change

--- kishorebiodata/js.js
+++ kishorebiodata/js.js
@@ -1,5 +1,5 @@
 document.getElementById("showBtn").addEventListener("click", function () {
-  const details = document.getElementById("moredetails");
+  const details = document.getElementById("moreDetails");
 
   if (details.style.display === "none") {
     details.style.display = "block";
